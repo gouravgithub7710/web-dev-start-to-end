91cbcbb067642319b1970f089b66347e09195ecf
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-19 00:04:33 +0530

    react start --

--- React-start/src/App.jsx
+++ React-start/src/App.jsx
@@ -1,8 +1,10 @@
 function app()
 {
+  const a = 10;
+  const name ="Gourav";
   return <div>
-    <h1>My React App</h1>
-    <p>This is a simple React application.</p>
+    <h1>My React App-- {a}</h1>
+    <p>This is a simple React application,made by--{name}</p>
     <p>It is built using Vite and React.</p>
   </div>
 }
