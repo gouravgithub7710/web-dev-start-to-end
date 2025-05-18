f7cf1db2cd56fa1692ee1f3f6c4f41a4306586a3
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-19 00:07:02 +0530

    react start - function use

--- React-start/src/App.jsx
+++ React-start/src/App.jsx
@@ -2,10 +2,14 @@ function app()
 {
   const a = 10;
   const name ="Gourav";
+  const age = 21;
   return <div>
     <h1>My React App-- {a}</h1>
     <p>This is a simple React application,made by--{name}</p>
     <p>It is built using Vite and React.</p>
+
+
+    <p>Hello ji-- name is {name}, and age is {age}</p>
   </div>
 }
 
