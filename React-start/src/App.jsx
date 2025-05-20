88a32702eb938e6e85ca83a32715a26ff45009f2
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-20 17:10:40 +0530

    change user name

--- React-start/src/App.jsx
+++ React-start/src/App.jsx
@@ -3,6 +3,17 @@ function app()
   const a = 10;
   const name ="Gourav";
   const age = 21;
+let username = "John_user";
+
+  const changeusername =()=>
+  {
+    console.log(username);
+    
+     username="John_new_user";
+     
+     console.log(username);
+     
+  }
   return <div>
     <h1>My React App-- {a}</h1>
     <p>This is a simple React application,made by--{name}</p>
@@ -10,6 +21,9 @@ function app()
 
 
     <p>Hello ji-- name is {name}, and age is {age}</p>
+
+    <h1>User name is: {username}</h1>
+    <button onClick={changeusername}>changeuser</button>
   </div>
 }
 
