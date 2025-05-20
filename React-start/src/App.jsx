d5e67010cc840a71410453b5c12aeaef8f067586
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-20 18:06:53 +0530

    change value

--- React-start/src/App.jsx
+++ React-start/src/App.jsx
@@ -1,30 +1,51 @@
-function app()
-{
-  const a = 10;
-  const name ="Gourav";
-  const age = 21;
-let username = "John_user";
+// function app()
+// {
+//   const a = 10;
+//   const name ="Gourav";
+//   const age = 21;
+// let username = "John_user";
 
-  const changeusername =()=>
-  {
-    console.log(username);
+//   const changeusername =()=>
+//   {
+//     console.log(username);
     
-     username="John_new_user";
+//      username="John_new_user";
      
-     console.log(username);
+//      console.log(username);
      
+//   }
+//   return <div>
+//     <h1>My React App-- {a}</h1>
+//     <p>This is a simple React application,made by--{name}</p>
+//     <p>It is built using Vite and React.</p>
+
+
+//     <p>Hello ji-- name is {name}, and age is {age}</p>
+
+//     <h1>User name is: {username}</h1>
+//     <button onClick={changeusername}>changeuser</button>
+//   </div>
+// }
+
+// export default app;
+
+
+import React, { useState } from 'react';
+
+const App=()=>{
+  const [a, seta] = useState(10);
+
+  const changeA=()=>{
+    console.log(a);
+    seta(20);
+    console.log(a); 
   }
-  return <div>
-    <h1>My React App-- {a}</h1>
-    <p>This is a simple React application,made by--{name}</p>
-    <p>It is built using Vite and React.</p>
-
-
-    <p>Hello ji-- name is {name}, and age is {age}</p>
-
-    <h1>User name is: {username}</h1>
-    <button onClick={changeusername}>changeuser</button>
-  </div>
+  return (
+    <div>
+    <h1>Value of a is:{a}</h1>
+    <button onClick={changeA}>Change A</button>
+    </div>
+  );
 }
+export default App; 
 
-export default app;
