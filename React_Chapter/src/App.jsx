0888a7e0104f975bf88c0500326e8283f43928ec
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-13 01:03:35 +0530

    use onclick

--- React_Chapter/src/App.jsx
+++ React_Chapter/src/App.jsx
@@ -26,46 +26,46 @@
 
 //use onsubmit to handle form submission
 
-import React from 'react'
-
-const App = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted");
-    console.log(e.target[0].value); // Accessing the first input value (email)
-    console.log(e.target[1].value); // Accessing the second input value (password)
-    
-  }
-
-  return (
-    <div>
-      <h1>Fill the form</h1>
-      <form onSubmit={handleSubmit}>
-
-        <input type="email" placeholder="Enter your email"/>
-        <input type="password" placeholder="Enter your password"/>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  )
-}
-
-export default App
-
-//use onclick to handle button click
-// import React from 'react'     
+// import React from 'react'
 
 // const App = () => {
-//   const handleClick = () => {    
-//     console.log("Button clicked");
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log("Form submitted");
+//     console.log(e.target[0].value); // Accessing the first input value (email)
+//     console.log(e.target[1].value); // Accessing the second input value (password)
+    
 //   }
-//   return (
 
+//   return (
 //     <div>
-//       <h1>Click the button</h1>
-//       <button onClick={handleClick}>Click Me</button>
+//       <h1>Fill the form</h1>
+//       <form onSubmit={handleSubmit}>
+
+//         <input type="email" placeholder="Enter your email"/>
+//         <input type="password" placeholder="Enter your password"/>
+//         <button type="submit">Submit</button>
+//       </form>
 //     </div>
-//   )    
+//   )
 // }
+
 // export default App
+
+//use onclick to handle button click
+import React from 'react'     
+
+const App = () => {
+  const handleClick = () => {    
+    console.log("Button clicked");
+  }
+  return (
+
+    <div>
+      <h1>Click the button</h1>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  )    
+}
+export default App
 
