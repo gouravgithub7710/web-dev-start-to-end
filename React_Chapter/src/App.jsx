82b11c917cf7099ef71e1ff1d8a5e629e06c3ec3
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-14 00:06:46 +0530

    product details

--- React_Chapter/src/App.jsx
+++ React_Chapter/src/App.jsx
@@ -71,6 +71,7 @@
 
 //rafe for react components
 //const implement up side of the return statement
+//<></> empty open and close tags 
 import React from 'react'
 
 const App = () => {
@@ -80,16 +81,23 @@ const App = () => {
     age: 30,
     email: "John@gmail.com"}
 
+    const product = {
+      name: "Laptop",
+      price: 999.99,
+      inStock: true
+    };
+
 
   return (
-    <div>
+    <>
+<div>
     <h1>APP</h1>
 
     <h2>Course: {course}</h2>
     <p>This is a simple React component.</p>
     {/* <p>Welcome to the course!</p>
     <p>Enjoy learning React!</p> */}
-<>------------------------------------------------------</>
+   <>------------------------------------------------------</>
     <h3>Person Details:</h3>
     <p>Name: {person.name}</p>
     <p>Age: {person.age}</p>
@@ -98,6 +106,19 @@ const App = () => {
     <p>Have a great day!</p>
     <p>Feel free to explore more!</p>
     </div>
+    <>------------------------------------------------------</>
+    <div>
+    <h3>Product Details:</h3>
+    <p>Name: {product.name}</p>
+    <p>Price: {product.price}</p>
+    <p>In Stock: {product.inStock ? "Yes" : "No"}</p>
+    <p>Check out our latest products!</p>
+    <p>Don't miss out on our special offers!</p>
+    <p>Shop now and save big!</p>
+    <p>Thank you for visiting our store!</p>
+    </div>
+    </>
+    
   )
 }
 
