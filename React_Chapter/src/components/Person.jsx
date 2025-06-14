9e8dabebe717c9e9a7bff7bffd4aec36b0e03ede
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-14 18:45:14 +0530

    person component

--- React_Chapter/src/components/Person.jsx
+++ React_Chapter/src/components/Person.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Person = ({name,age}) => {
  const check= () => {if(age>18){alert("You can drive")}else{ alert("You can not drive")}}
   return (
-    <div>
+    <div style={{backgroundColor:"lightblue",padding:"20px",borderRadius:"10px",color:"blue", textAlign:"center", border:"2px solid yellow", width:"300px" }}>
     <h1>Name={name}</h1>
     <h1>age={age}</h1>
 
-    <button onClick={check}>Check drive or not</button>
+    <button style={{color:"blue" ,backgroundColor:"lightgreen",width:"200px", fontSize:"20px", borderRadius: "15px" }} onClick={check}>Check drive or not</button>
 
     </div>
   )
