e269705a6bf2c2c3f3d2a32b7982f765555a12c6
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-17 00:27:40 +0530

    onClick for additon for fn parameter

--- React_Chapter/src/components/Events.jsx
+++ React_Chapter/src/components/Events.jsx
@@ -4,12 +4,17 @@ const Events = () => {
   const handleClick = () => {
     alert('Button clicked!');
   };
+  const addition=(a) => {
+     alert(a + 10);
+  }
   return (
     <div>
     <h1>Event Handling Example</h1>
-      <button className='button' onClick={handleClick}>Click Me</button>
-      <button className='button' onMouseOver={handleClick}>Mouse hover</button>
+      <button className='button' onClick={handleClick}>Click Me B1</button>
+      <button className='button' onMouseOver={handleClick}>Mouse hover B2</button>
+      <button className='button' onClick={()=>addition(100)}>Addition B3</button>
     </div>
+    
   )
 }
 
