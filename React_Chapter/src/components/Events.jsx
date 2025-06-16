6ef996d1d5594d9ad35cdcb80ed26ad971512afd
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-17 00:18:52 +0530

    onClick and onMouseOver event perform

--- React_Chapter/src/components/Events.jsx
+++ React_Chapter/src/components/Events.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Events = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
   return (
     <div>
-      
+    <h1>Event Handling Example</h1>
+      <button className='button' onClick={handleClick}>Click Me</button>
+      <button className='button' onMouseOver={handleClick}>Mouse hover</button>
     </div>
   )
 }
