699daba2951bb57c136b032a2b992a1f66ca28d0
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-24 23:50:01 +0530

    Update Fetch using useeffect

--- React_Chapter/src/App.jsx
+++ React_Chapter/src/App.jsx
@@ -273,15 +273,15 @@ import './index.css'
 
 //fetch data from API using useEffect hook
 
-import React from 'react'
-import Fetch_Data_API from './components/Fetch_Data_API'
+// import React from 'react'
+// import Fetch_Data_API from './components/Fetch_Data_API'
 
-const App=()=>{
-  return(
-    <>
-      <Fetch_Data_API/>
-    </>
-  )
-}
+// const App=()=>{
+//   return(
+//     <>
+//       <Fetch_Data_API/>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
