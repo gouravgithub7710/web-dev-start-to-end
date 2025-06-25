6d8ab52290234e1b89f744a5925816144cab2189
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-25 23:59:36 +0530

    Todo Item list Components

---
+++ Projects/Simple-TODO-Components/src/Components/TodoIteam.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const TodoIteam=()=>{
+return(
+  <div className='Todo-Iteam'>
+    <input type="checkbox" />
+    <p>Eat</p>
+    <p>...</p>
+  </div>
+)
+}
+export default TodoIteam;
