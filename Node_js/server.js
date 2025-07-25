008bac7bb7b1ad18e6c0d10a71bed69e28dfe72a
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-26 00:53:59 +0530

    server comments

--- Node_js/server.js
+++ Node_js/server.js
@@ -11,5 +11,8 @@
 // The above code is a simple function to add two numbers and log the result.
 // The commented-out import statement suggests that this code might be part of a larger module system.
 
+// If you want to use the add function from another file, you can uncomment the import statement
+// and ensure that the file is set up to support ES6 modules, or use a bund
+
 
  
