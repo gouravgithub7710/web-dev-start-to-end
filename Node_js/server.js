fd9757099ce869145be1634ba349009c51906611
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-07-27 23:51:04 +0530

    require  os and fs

--- Node_js/server.js
+++ Node_js/server.js
@@ -1,28 +1,45 @@
- function add(a,b){
-  return a+b;
- }
+//  function add(a,b){
+//   return a+b;
+//  }
 
- var result =add(5,10);
- console.log(result);
+//  var result =add(5,10);
+//  console.log(result);
 
-var addidation = function(a,b){
-  return a+b;
-}
+// var addidation = function(a,b){
+//   return a+b;
+// }
 
-console.log(addidation(20,30));
+// console.log(addidation(20,30));
 
-var addation2digit = (a,b) => {
-  return a+b;
-}
-console.log(addation2digit(100,200));
+// var addation2digit = (a,b) => {
+//   return a+b;
+// }
+// console.log(addation2digit(100,200));
  
 
-(function(a,b){
- console.log("Anonymous function result: " + (a + b));
+// (function(a,b){
+//  console.log("Anonymous function result: " + (a + b));
  
-})(10,20);
+// })(10,20);
 
-(function callback()
-{
-  console.log("This is a callback function");
-})();
+// (function callback()
+// {
+//   console.log("This is a callback function");
+// })();
+
+const { log } = require('console');
+var fs = require('fs');
+var os = require('os');
+
+var user = os.userInfo();
+console.log(user.username);
+
+console.log(os);
+
+
+fs.appendFile('greeting.txt', `Hello ${user.username}!\n`, () => {
+  console.log('File created successfully!');
+});
+
+console.log(fs);
+
